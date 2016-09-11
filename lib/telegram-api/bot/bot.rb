5b22d914bb3778c0257b1341cb7910b4bf4882fa
Georgiy Odisharia <math.kraut.cat@gmail.com>
2016-09-11 21:22:14 +0300

    Initial commit

---
+++ lib/telegram-api/bot/bot.rb
@@ -0,0 +1,19 @@
+module TelegramBot
+  class Bot
+    API_URL = 'https://api.telegram.com'
+
+    def initialize(args = {})
+      # compatibility with just passing a token
+      if args.is_a?(String)
+        args = { token: args }
+      end
+
+      @token      = opts.fetch(:token)
+      @timeout    = opts[:timeout]  || 50
+      @offset     = opts[:offset]   || 0
+      @logger     = opts[:logger]   || NullLogger.new
+      @proxy      = opts[:proxy]    || nil
+      @connection = Excon.new(API_URL, persistent: true, proxy: @proxy)
+    end
+
+
